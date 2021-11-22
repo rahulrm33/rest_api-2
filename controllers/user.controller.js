334b449ebbda9eb9b27fbf453ef749727d95d814
rahulrm33 <rahavarahul23@gmail.com>
2021-11-22 20:11:27 +0530

    minor changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,7 +13,7 @@ async function Register(req, res){
         const saved_user = await user.save();
         res.json({status: true, message: "Registered successfully.", data: saved_user});
     } catch (error) {
-        // do logging in DB or file.
+        
         res.status(400).json({status: false, message: "Something went wrong.", data: error});
     }
 }
